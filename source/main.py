74d07411e0367d74e41f439926ad451c682b3b05
Ковалев Егор <pulseefast@gmail.com>
2024-10-20 18:10:32 +0700

    quote gen

--- source/main.py
+++ source/main.py
@@ -8,8 +8,23 @@ import dotenv
 dotenv.load_dotenv()
 bot_token = os.getenv("TG_BOT_KEY")
 
-bot = Bot(token=bot_token) # type: ignore
+bot = Bot(token=bot_token)  # type: ignore
 dp = Dispatcher()
+
+
+@dp.message()
+async def get_quote(msg: types.Message):
+    if msg.text == "Quote":
+        try:
+            data = requests.get("https://zenquotes.io/api/random").json()
+            await msg.answer(
+                f'Quote: {data[0]["q"]}\nAuthor: {data[0]["a"]}',
+            )
+        except Exception as err:
+            await msg.answer(f"Something went wrong\nError: {err}")
+    else:
+        await msg.answer("Ты хочешь что-то еще?")
+
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
