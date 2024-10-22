ab0b9f1b8738e43d85d7d764d526a6f0e3ac0dee
Ковалев Егор <pulseefast@gmail.com>
2024-10-22 14:55:31 +0700

    transfer main

---
+++ main.py
@@ -0,0 +1,58 @@
+import asyncio
+import os
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
+import logging
+import requests
+import dotenv
+from source.routers import router as handlers_router
+
+dotenv.load_dotenv()
+bot_token = os.getenv("TG_BOT_KEY")
+db_url = os.getenv("TG_BOT_DB")
+print(bot_token)
+bot = Bot(
+    token=bot_token,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)  # type: ignore
+dp = Dispatcher()
+dp.include_router(handlers_router)
+
+
+@dp.message(Command("create_task"))
+async def create_task(msg: types.Message):
+    await msg.answer(msg.from_user.id, "Good!\nWrite the name of your task")
+
+
+# TODO: аналогично start
+@dp.message(Command("help"))
+async def handle_help(msg: types.Message):
+    pass
+
+
+# TODO: поприветствовать + отослать к комманде help
+@dp.message(CommandStart())
+async def handle_start(msg: types.Message):
+    await msg.answer(f"Hello {msg.from_user.full_name}!")
+
+
+@dp.message(Command("quote"))
+async def get_quote(msg: types.Message):
+    try:
+        data = requests.get("https://zenquotes.io/api/random").json()
+        await msg.answer(
+            f'Quote: {data[0]["q"]}\nAuthor: {data[0]["a"]}',
+        )
+    except Exception as err:
+        await msg.answer(f"Something went wrong\nError: {err}")
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
